6c5dde68a20ad5e3feb231cbd8a5c820f45bfc73
Dimitar Pavlovski <86356807+Dpavlovski@users.noreply.github.com>
2023-04-02 20:04:00 +0200

    Add files via upload

---
+++ backend/src/main/java/mk/ukim/finki/emt/lab/EMT_Lab1and2.java
@@ -0,0 +1,21 @@
+package mk.ukim.finki.emt.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class EMT_Lab1and2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMT_Lab1and2.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+}
